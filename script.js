9e33ae4adba9940abb5f54d2b187f89c93418daf
Sadhna kumari <sadhnakhairia1998@gmail.com>
2022-08-26 21:33:33 +0530

    recipies

---
+++ script.js
@@ -0,0 +1,19 @@
+const input = document.querySelector(".inputClass");
+const submitBtn = document.querySelector(".submitButton");
+
+submitBtn.addEventListener('click', function(event){
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
+    .then(resolve => resolve.json())
+    .then(data =>{
+        const dishes = data["meals"]
+        console.log(dishes)
+        // console.log(data)
+        dishes.map((meal) =>{
+            let result = `<div><h1>Name : ${meal.strMeal}</h1>
+            <h1>Instructions</h1> : 
+            <p>${meal.strInstructions}</p>/div>`
+        let container = document.querySelector(".container")
+        container.innerHTML += result
+        })
+    }).catch(err => alert("error"))
+})
